00a5ee109b3ccf6847bd3115578d62ee36262765
June <chonnipa.kit@gmail.com>
2025-01-22 03:11:54 +0700

    Config linter and formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,66 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import perfectionist from "eslint-plugin-perfectionist";
+import prettier from "eslint-plugin-prettier/recommended";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+const config = tseslint.config(
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,cjs,mjs,ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      perfectionist.configs["recommended-natural"],
+    ],
+    rules: {
+      "perfectionist/sort-enums": "off",
+      "perfectionist/sort-interfaces": "off",
+      "perfectionist/sort-intersection-types": "off",
+      "perfectionist/sort-jsx-props": "off",
+      "perfectionist/sort-maps": "off",
+      "perfectionist/sort-object-types": "off",
+      "perfectionist/sort-objects": "off",
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["*.js"],
+          defaultProject: "tsconfig.json",
+        },
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react,
+      // TODO: Upgrade eslint-plugin-react-hooks to newer version when this type
+      // error fix is released: https://github.com/facebook/react/pull/30774
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      "react-hooks/exhaustive-deps": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
+    settings: { react: { version: "19.0" } },
   },
-)
+  prettier,
+);
+
+export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App.tsx";
+
+const container = document.getElementById("root");
+
+if (!container) throw new Error("root not found");
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
